b83eebe3868000f5782d107fe1a941326f2750eb
Itunu Lamina <nix4phun@gmail.com>
2023-01-19 22:41:36 +0100

    chore: add popover on T and C

--- ice-cream/src/pages/summary/test/SummaryForm.test.jsx
+++ ice-cream/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import SummaryForm from '../SummaryForm';
 
 describe('SummaryForm', () => {
@@ -11,7 +12,8 @@ describe('SummaryForm', () => {
 			expect(checkbox).not.toBeChecked();
 		});
 
-		it('should enable order button when checked and disable order button when unchecked', () => {
+		it('should enable order button when checked and disable order button when unchecked', async () => {
+			const user = userEvent.setup();
 			render(<SummaryForm />);
 
 			const button = screen.getByRole('button', {
@@ -23,11 +25,33 @@ describe('SummaryForm', () => {
 
 			expect(button).toBeDisabled();
 
-			fireEvent.click(checkbox);
+			await user.click(checkbox);
 			expect(button).toBeEnabled();
 
-			fireEvent.click(checkbox);
+			await user.click(checkbox);
 			expect(button).toBeDisabled();
+		});
+
+		it('should popover on hover', async () => {
+			const user = userEvent.setup();
+
+			render(<SummaryForm />);
+            
+			const nullPopover = screen.queryByText(
+				/no ice cream will actually be delivered/i
+			);
+			expect(nullPopover).not.toBeInTheDocument();
+
+			const termsAndConditions =
+				screen.getByText(/terms and conditions/i);
+			await user.hover(termsAndConditions);
+			const popover = screen.getByText(
+				/no ice cream will actually be delivered/i
+			);
+			expect(popover).toBeInTheDocument();
+
+			await user.unhover(termsAndConditions);
+			expect(popover).not.toBeInTheDocument();
 		});
 	});
 });
