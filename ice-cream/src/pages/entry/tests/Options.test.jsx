fdc67e1a2c15c61a72e313482a9f49e2d04f9283
Itunu Lamina <nix4phun@gmail.com>
2023-01-20 11:46:24 +0100

    update

--- ice-cream/src/pages/entry/tests/Options.test.jsx
+++ ice-cream/src/pages/entry/tests/Options.test.jsx
@@ -13,4 +13,20 @@ describe('Options', () => {
 		const getAlt = scoopImages.map((element) => element.alt);
 		expect(getAlt).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 	});
+
+	it('should display image for each topping option from the server', async () => {
+		render(<Options optionType='toppings' />);
+
+		const toppingImages = await screen.findAllByRole('img', {
+			name: /topping$/i,
+		});
+		expect(toppingImages).toHaveLength(3);
+
+		const imageTitles = toppingImages.map((element) => element.alt);
+		expect(imageTitles).toEqual([
+			'Cherries topping',
+			'M&Ms topping',
+			'Hot Fudge topping',
+		]);
+	});
 });
